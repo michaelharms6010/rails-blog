8d99c072b3d7de2f16060272a329f1ead7bb5f65
Michael Harms <michael.harms6010@gmail.com>
2020-06-15 16:07:52 -0500

    adds comment handling

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   get "about" => "pages#about", as: "about"
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
