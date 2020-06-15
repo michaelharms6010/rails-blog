da450edb9ac745ed1f380fbf96fccd726b32060f
Michael Harms <michael.harms6010@gmail.com>
2020-06-15 15:51:28 -0500

    adds comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  root "posts#index"
+  root "posts#index", as: "home"
 
-  get "about" => "pages#about"
+  get "about" => "pages#about", as: "about"
+
+  resources :posts
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
